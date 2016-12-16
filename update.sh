f49040cb7629cd4fe30c603b8dd06dab28a9f16b
conni <c.buschka@arcusx.com>
2016-12-16 10:17:04 +0100

    Script to update meta data.

---
+++ update.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+PROJECT_DIR=$(cd `dirname $0`; pwd)
+
+for d in apt/ubuntu; do
+	cd $PROJECT_DIR/$d
+	for v in dists/xenial/unstable; do
+		dpkg-scanpackages $v/binary-amd64/ > $v/binary-amd64/Packages
+	done
+done
+
+for f in $(find . -name "Packages"); do 
+	bzip2 -c $f > $f.bz2
+	gzip -c $f >$f.gz
+done
