4473196af386fc616178c7b0075f9f50b35f183a
conni <c.buschka@arcusx.com>
2016-12-16 17:05:38 +0100

    Added new xenial and yakkety packages for mechanic 0.1.

--- update.sh
+++ update.sh
@@ -4,7 +4,7 @@ PROJECT_DIR=$(cd `dirname $0`; pwd)
 
 for d in apt/ubuntu; do
 	cd $PROJECT_DIR/$d
-	for v in dists/xenial/unstable; do
+	for v in dists/xenial/unstable dists/yakkety/unstable; do
 		dpkg-scanpackages $v/binary-amd64/ > $v/binary-amd64/Packages
 	done
 done
