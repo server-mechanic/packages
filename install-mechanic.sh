d8882d5d4ecc4b1bcd9da529c10567861768a64a
conni <c.buschka@arcusx.com>
2017-01-14 15:43:00 +0100

    Added simple install script.

---
+++ install-mechanic.sh
@@ -0,0 +1,86 @@
+#!/bin/bash
+
+channel=$1
+if [[ "unstable" != "${channel}" ]]; then
+  echo "The only channel is unstable for now. So call it $0 unstable"
+  exit 1
+fi
+
+if [[ "$UID" != "0" ]]; then
+  echo "Must be run as root."
+  exit 1
+fi
+
+function install_debian_based() {
+  dist=$1
+  release=$2
+
+  apt-get update && apt-get install -y apt-transport-https
+cat - >/etc/apt/sources.list.d/server-mechanic-unstable.list <<EOB
+deb [trusted=yes] https://raw.githubusercontent.com/server-mechanic/packages/master/apt/${dist}/ ${release} ${channel}
+EOB
+cat /etc/apt/sources.list.d/server-mechanic.list
+  apt-get update && apt-get install -y mechanic
+
+  /usr/sbin/mechanic version
+}
+
+if [[ -f "/etc/lsb-release" ]]; then
+  source /etc/lsb-release
+  case "$DISTRIB_ID$DISTRIB_CODENAME" in
+    Ubuntuxenial)
+      install_debian_based ubuntu xenial
+    ;;
+    Ubuntuyakkety)
+      install_debian_based ubuntu yakkety
+    ;;
+    *)
+      echo "Unsupported lsb linux $DISTRIB_ID $DISTRIB_CODENAME."
+      exit 1
+    ;;
+  esac
+elif [[ -f "/etc/debian_version" ]]; then
+  debian_version=$(cat /etc/debian_version)
+  case $debian_version in
+    *8.*|*jessie*)
+      echo "Sorry debian jessie support is currently broken."
+      exit 1
+    ;;
+    stretch/sid)
+      install_debian_based debian sid
+    ;;
+    *)
+      echo "Unsupported debian. ($debian_version)"
+      exit 1
+    ;;
+  esac
+elif [[ -f "/etc/fedora-release" ]]; then
+  fedora_version=$(cat /etc/fedora-release)
+  case $fedora_version in
+    *25*)
+      echo "Fedora 25 is currently not supported. ($fedora_version)"
+      exit 1
+    ;;
+    *)
+      echo "Unsupported fedora. ($fedora_version)"
+      exit 1
+    ;;
+  esac
+elif [[ -f "/etc/redhat-release" ]]; then
+  redhat_version=$(cat /etc/redhat-release)
+  case $redhat_version in
+    *CentOS*7.*)
+      echo "CentOS is currently not supported. ($redhat_version)"
+      exit 1
+    ;;
+    *)
+      echo "Unsupported redhat. ($redhat_version)"
+      exit 1
+    ;;
+  esac
+else
+  echo "Unsupported linux."
+  exit 1
+fi
+
+exit 0
