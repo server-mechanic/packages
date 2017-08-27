a79785649d3bf18e37ee3178ecf7cfa29cb920ca
conni <c.buschka@arcusx.com>
2017-08-27 10:36:15 +0200

    Added verification of debian:stretch, fedora:26 and ubuntu:zesty.

--- scripts/verify.sh
+++ scripts/verify.sh
@@ -5,22 +5,29 @@ PROJECT_DIR=$(cd `dirname $0`/..; pwd)
 function verify() {
   local d=$1
   curl -s https://raw.githubusercontent.com/server-mechanic/packages/master/install-mechanic.sh \
-    | docker run -i $d /bin/bash -s unstable
+    | docker run -i $d /bin/bash -x -s unstable
 }
+
+result=""
 
 if [[ ! -z "$1" ]]; then
   verify $1
 else
-  for d in debian:sid debian:wheezy ubuntu:xenial ubuntu:yakkety fedora:25 centos:7; do
+  for d in debian:sid debian:stretch debian:jessie debian:wheezy ubuntu:xenial ubuntu:yakkety ubuntu:zesty fedora:25 fedora:26 centos:7; do
     verify $d
     exit_code=$?
     if [ "$exit_code" != "0" ]; then
-      echo "$d FAILED with $exit_code."
+      result="$result $d(FAILED)"
       exit 1
     else
-      echo "$d OK."
+      result="$result $d(OK)"
     fi
   done 
+fi
+echo $result
+
+if [ $(echo "$result" | grep "FAILED") ]; then
+ exit 1
 fi
 
 exit 0
